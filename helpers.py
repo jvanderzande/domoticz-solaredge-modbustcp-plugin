4b50c7755122da4f246ae25bb38adf7f5de596e9
jvanderzande <jvdzande@yahoo.com>
2025-06-16 19:17:56 +0200

    - Removed Hardware components selection and determine what is attached - Added P1-Sync like active_power_limit branch - Removed "Current" device for 1-fase as that is the same as "L1 Current" - Added the DSTATUS & DERROR log option.

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 import Domoticz
 
 from datetime import datetime
+import time
 from enum import IntEnum, unique
 
 #
@@ -13,7 +14,9 @@ class LogLevels(IntEnum):
     NORMAL      = 0
     VERBOSE     = 1
     EXTRA       = 2
-    MAX         = 3
+    DEBUG       = 3
+    DSTATUS     = 101
+    DERROR      = 102
 
 CurrentLogLevel = LogLevels.NORMAL
 
@@ -22,7 +25,11 @@ def SetLogLevel(level):
     CurrentLogLevel = level
 
 def DomoLog(level, message):
-    if (CurrentLogLevel >= level):
+    if (LogLevels.DSTATUS == level):
+        Domoticz.Status(message)
+    elif (LogLevels.DERROR == level):
+        Domoticz.Status(message)
+    elif (CurrentLogLevel >= level):
         Domoticz.Log(message)
 
 #
@@ -79,7 +86,7 @@ class Average:
         while (len(self.samples) > self.max_samples):
             del self.samples[0]
 
-        DomoLog(LogLevels.MAX, "Average: {} - {} values".format(self.get(), len(self.samples)))
+        DomoLog(LogLevels.DEBUG, "Average: {} - {} values".format(self.get(), len(self.samples)))
 
     def get(self):
         return sum(self.samples) / len(self.samples)
@@ -108,7 +115,61 @@ class Maximum:
         while (len(self.samples) > self.max_samples):
             del self.samples[0]
 
-        DomoLog(LogLevels.MAX, "Maximum: {} - {} values".format(self.get(), len(self.samples)))
+        DomoLog(LogLevels.DEBUG, "Maximum: {} - {} values".format(self.get(), len(self.samples)))
 
     def get(self):
         return max(self.samples)
+
+class UpdatePeriod:
+    def __init__(self):
+        self.samples = []
+        self.max_samples = 5
+        self.prev_update_time = None  # This is used to skip the first reading as that could be old when Domoticz was down
+        self.last_update_time = None  # This is the first reading we consider as start value
+
+    def set_max_samples(self, max_samples):
+        self.max_samples = max(1, max_samples)  # Ensure at least 1 sample
+
+    def update(self, new_value):
+        # Convert string to datetime
+        # current_time = datetime.strptime(new_value, "%Y-%m-%d %H:%M:%S")
+        updtime = time.strptime(new_value, "%Y-%m-%d %H:%M:%S")
+        current_time = datetime.fromtimestamp(time.mktime(updtime))
+
+        # Check if new_value is the same as the last recorded timestamp
+        if self.last_update_time and current_time == self.last_update_time:
+            return  # No update needed
+
+
+        # If there is a previous timestamp, calculate difference between the p
+        if self.prev_update_time is not None:
+            time_diff = (current_time - self.last_update_time).total_seconds()
+            self.samples.append(time_diff)
+
+            # Keep samples within the max limit
+            while len(self.samples) > self.max_samples:
+                self.samples.pop(0)
+        # Update last timestamp
+        self.prev_update_time = self.last_update_time
+        self.last_update_time = current_time
+
+    def get(self):
+        if not self.samples:
+            return 0.0  # No data yet
+        return sum(self.samples) / len(self.samples)  # Compute average
+
+    def seconds_last_update(self):
+        """Returns seconds between last update and now"""
+        if not self.last_update_time:
+            return None  # No previous update
+        return (datetime.now() - self.last_update_time).total_seconds()
+
+    def count(self):
+        return len(self.samples)
+
+    def initdone(self):
+        return (self.last_update_time != None)
+
+    def reset(self):
+        self.samples.clear()
+        self.last_update_time = None
