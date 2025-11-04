157572174905c304476ba4650a5dc7d254599f94
jvanderzande <jvdzande@yahoo.com>
2025-11-04 09:45:27 +0100

    fix 3fase Selector switch type for "Storage AC Charge Policy"

--- inverters.py
+++ inverters.py
@@ -58,7 +58,7 @@ SINGLE_PHASE_INVERTER = [
     [InverterUnit.COSPHI,             "cos-phi",            0xF3, 0x13,    0x00,       {},                  "cosphi",             None,                   "{}",     None,        None,         None,        None,                                 None      ],
     [InverterUnit.STORAGECONTROL,"Storage Control Mode",0xF4,0x49,0x12,{"LevelActions": "||||"  ,"LevelNames": "Disabled|Maximize Self Consumption|Time of Use|Backup Only|Remote Control","LevelOffHidden": "false","SelectorStyle": "1"} , "storage_control_mode",None,"{:.0f}", None,None,None,None,None, 30],
     [InverterUnit.RCCMDMODE,     "Remote Control Mode", 0xF4,0x49,0x12,{"LevelActions": "|||||||","LevelNames": "Off|Charge from excess PV power only|Charge from PV first|Charge from PV and AC|Maximize export|Discharge to match load|unused|Maximize self consumption","LevelOffHidden": "false","SelectorStyle": "1"}, "rc_cmd_mode"         ,None,"{:.0f}", None,None,None,None,None, 30],
-    [InverterUnit.STORAGE_AC_CHARGE_POLICY, "Storage AC Charge Policy", 0xF4,0x49,0x12, {"LevelNames":"Disabled|Always Allowed|Fixed Energy Limit|Percent of Production","LevelOffHidden":"false","SelectorStyle":"1"}, "storage_ac_charge_policy", None, "{}", None, None, None, None, None]
+    [InverterUnit.STORAGE_AC_CHARGE_POLICY, "Storage AC Charge Policy", 0xF4,0x49,0x12, {"LevelActions": "||||", "LevelNames":"Disabled|Always Allowed|Fixed Energy Limit|Percent of Production","LevelOffHidden":"false","SelectorStyle":"1"}, "storage_ac_charge_policy", None, "{}", None, None, None, None, None]
 ]
 
 THREE_PHASE_INVERTER = [
@@ -90,7 +90,7 @@ THREE_PHASE_INVERTER = [
     [InverterUnit.COSPHI,             "cos-phi",            0xF3, 0x13,    0x00,       {},                  "cosphi",             None,                   "{}",     None,        None,         None,        None,                                 None      ],
     [InverterUnit.STORAGECONTROL,"Storage Control Mode",0xF4,0x49,0x12,{"LevelActions": "||||","LevelNames": "Disabled|Maximize Self Consumption|Time of Use|Backup Only|Remote Control","LevelOffHidden": "false","SelectorStyle": "1"}, "storage_control_mode",None,"{:.0f}", None,None,None,None,None, 30],
     [InverterUnit.RCCMDMODE,     "Remote Control Mode", 0xF4,0x49,0x12,{"LevelActions": "|||||||","LevelNames": "Off|Charge from excess PV power only|Charge from PV first|Charge from PV and AC|Maximize export|Discharge to match load|NotUsed|Maximize self consumption","LevelOffHidden": "false","SelectorStyle": "1"}, "rc_cmd_mode"         ,None,"{:.0f}", None,None,None,None,None, 30],
-    [InverterUnit.STORAGE_AC_CHARGE_POLICY, "Storage AC Charge Policy", 0xF4, 0x12, 0x00, {"LevelNames":"Disabled|Always Allowed|Fixed Energy Limit|Percent of Production","LevelOffHidden":"false","SelectorStyle":"1"}, "storage_ac_charge_policy", None, "{}", None, None, None, None, None]
+    [InverterUnit.STORAGE_AC_CHARGE_POLICY, "Storage AC Charge Policy", 0xF4,0x49,0x12, {"LevelActions": "||||","LevelNames":"Disabled|Always Allowed|Fixed Energy Limit|Percent of Production","LevelOffHidden":"false","SelectorStyle":"1"}, "storage_ac_charge_policy", None, "{}", None, None, None, None, None]
 ]
 #
 # This lists all implemented options, but an inverter may not return all of them.
